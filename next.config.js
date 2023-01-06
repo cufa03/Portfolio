3202fd67d702f3ca4634c90d5ede40195857b93d
Facundo Nadaf <facundo.nadaf@integrify.io>
2023-01-06 14:15:45 +0100

    deleted files

--- next.config.js
+++ next.config.js
@@ -1,6 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-}
+	reactStrictMode: true,
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
+
+// module.exports = {
+// 	images: {
+// 		remotePatterns: [
+// 			{
+// 				protocol: 'https',
+// 				hostname: 'example.com',
+// 				port: '',
+// 				pathname: '/account123/**',
+// 			},
+// 		],
+// 	},
+// };
+
+// https://cdn.sanity.io/images/ltuexkre/production/2a67945990f9c2ef568cf7e8483c1a8174556463-500x500.png
